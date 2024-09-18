0c0e38fd7fa29596e81c0d5ab6e5b2615790eafc
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2024-09-18 22:40:59 +0800

    Add dark/light theme swticher

--- layouts/new.vue
+++ layouts/new.vue
@@ -1,14 +1,46 @@
 <script setup lang="ts">
-import { Menu as MenuIcon, PersonOutline as PersonOutlineIcon, SettingsSharp as SettingsSharpIcon } from '@vicons/ionicons5'
+import { 
+    Menu as MenuIcon, 
+    PersonOutline as PersonOutlineIcon, 
+    SettingsSharp as SettingsSharpIcon,
+    SunnyOutline as LightThemeIcon,
+    Moon as DarkThemeIcon, 
+} from '@vicons/ionicons5'
+import { darkTheme } from 'naive-ui'
+import type { GlobalTheme } from 'naive-ui'
+
+
 const active = ref(false)
+
+
+const theme = ref<GlobalTheme | null>(null)
 
 const toggleHeader = () => {
     active.value = !active.value
 }
+
+const goToLogin = () => {
+    navigateTo('/login')
+}
+
+const handleChangeTheme = (value: string) => {
+    if(value == 'dark'){
+        theme.value = darkTheme
+    }
+    
+    else if(value == 'light'){
+        theme.value = null
+    }
+}
+
+
+
+
 </script>
 
 <template>
     <n-layout class="max-h-dvh h-dvh">
+        <n-config-provider :theme="theme">
         <n-drawer v-model:show="active" :width="502" placement="left" :block-scroll="false">
             <n-drawer-content title="Stoner">
             Stoner is a 1965 novel by the American writer John Williams.
@@ -24,21 +56,49 @@ const toggleHeader = () => {
                         </n-button>
                     </n-flex>
                     <n-flex justify="center" size="large" class="pr-6">
-                        <n-button text style="font-size: 24px" @click="toggleHeader">
-                            <n-icon>
-                                <PersonOutlineIcon/>
-                            </n-icon>
+                        <n-button text style="font-size: 24px" @click="goToLogin">
+                            <n-tooltip trigger="hover">
+                                <template #trigger>
+                                    <n-icon>
+                                        <PersonOutlineIcon/>
+                                    </n-icon>
+                                </template>
+                                Login
+                            </n-tooltip>
                         </n-button>
                         <n-button text style="font-size: 24px" @click="toggleHeader">
-                            <n-icon>
-                                <SettingsSharpIcon/>
-                            </n-icon>
+                            <n-tooltip trigger="hover">
+                                <template #trigger>
+                                    <n-icon>
+                                        <SettingsSharpIcon/>
+                                    </n-icon>
+                                </template>
+                                Settings
+                            </n-tooltip>
                         </n-button>
+                        <n-tooltip trigger="hover">
+                            <template #trigger>
+                                <n-switch size="large"
+                                    checked-value="dark"
+                                    unchecked-value="light"
+                                    @update:value="handleChangeTheme"
+                                >
+                                    <template #checked-icon>
+                                        <n-icon :component="DarkThemeIcon" />
+                                    </template>
+                                    <template #unchecked-icon>
+                                        <n-icon :component="LightThemeIcon" />
+                                    </template>
+                                </n-switch>
+                            </template>
+                            Switch Theme to {{ theme ? 'Light' : 'Dark' }}
+                        </n-tooltip>
                     </n-flex>
                 </n-flex>
-        </n-layout-header>
-      <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider class="h-full">
+            </n-layout-header>
+        <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider class="h-full">
         <slot></slot>
       </n-layout>
+        </n-config-provider>
     </n-layout>
   </template>
